ba42577966d4902932019bd022816e3d571ee478
Cheuk Hin (Kelvin) Lau <clau128@my.bcit.ca>
2017-05-23 02:18:01 -0700

    disabled closed functionality to allow for continued development

--- js/home.js
+++ js/home.js
@@ -24,37 +24,39 @@ $(document).ready(function(){
     });
     
     $(function(){
-        $("#menuScroller").click(function(){            
-            $.ajax({
-                url:"/openOrClosed",
-                type:"post", //"post" is behind the scenes (invisible) versus "get" (hijackable)
-                success:function(resp){
-                    if (resp.theStatus == true) {
+        $("#menuScroller").click(function(){  
+            /* DONT DELETE THESE COMMENTS */
+//            $.ajax({
+//                url:"/openOrClosed",
+//                type:"post", //"post" is behind the scenes (invisible) versus "get" (hijackable)
+//                success:function(resp){
+//                    if (resp.theStatus == true) {
                         location.href = "/menu";
-                    } else if (resp.theStatus == false) {
-                        alert("Sorry, we're currently closed");
-                    } else {
-                        alert("Couldn't read store status variable");
-                    }
-                }
-            });
+//                    } else if (resp.theStatus == false) {
+//                        alert("Sorry, we're currently closed");
+//                    } else {
+//                        alert("Couldn't read store status variable");
+//                    }
+//                }
+//            });
         });
     });
     
     $(function(){
         $(".spImage").click(function() {
-            $.ajax({
-                url:"/openOrClosed",
-                type:"post", //"post" is behind the scenes (invisible) versus "get" (hijackable)
-                success:function(resp){
-                    if (resp.theStatus == true) {
+            /* DONT DELETE THESE COMMENTS */
+//            $.ajax({
+//                url:"/openOrClosed",
+//                type:"post", //"post" is behind the scenes (invisible) versus "get" (hijackable)
+//                success:function(resp){
+//                    if (resp.theStatus == true) {
                         location.href = "/menu";
-                    } else if (resp.theStatus == false) {
-                        alert("Sorry, we're currently closed");
-                    } else {
-                        alert("Couldn't read store status variable");
-                    }
-                }
+//                    } else if (resp.theStatus == false) {
+//                        alert("Sorry, we're currently closed");
+//                    } else {
+//                        alert("Couldn't read store status variable");
+//                    }
+//                }
             });
         });
     });
